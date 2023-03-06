c7ccd5435d9062875dadea6209e822a7ef2017ed
vanxh <vanxhyt@gmail.com>
2023-03-07 03:00:07 +0530

    no nfts msg

--- apps/web/src/pages/nfts/[owner].tsx
+++ apps/web/src/pages/nfts/[owner].tsx
@@ -31,16 +31,27 @@ type ServerSideProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 export default function NFTs(props: ServerSideProps) {
   const [page] = useState(props.owner);
-  const [nfts, setNfts] = useState(props.nfts);
+  const [nfts, setNfts] = useState<any[]>(props.nfts);
 
   return (
     <div className="justify-start items-center flex flex-col min-h-[100vh] py-6 gap-y-6">
       <SearchBar />
 
-      <div className="flex flex-col items-center justify-center w-full h-full">
-        <h1 className="text-4xl md:text-6xl font-bold text-center w-[90%] mb-8">
-          {truncateAddress(props.owner)} owns {props.total} NFTs.
-        </h1>
+      <div className="flex flex-col items-center justify-start w-full h-full">
+        {nfts.length > 0 && (
+          <h1 className="text-4xl md:text-6xl font-bold text-center w-[90%] mb-8">
+            {truncateAddress(props.owner)} owns {props.total} NFTs.
+          </h1>
+        )}
+
+        {!nfts.length && (
+          <div className="text-center flex flex-col gap-y-2 items-center">
+            <span className="text-6xl">😆</span>
+            <p className="font-bold text-xl">
+              {truncateAddress(props.owner)} has 0 NFTs
+            </p>
+          </div>
+        )}
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-[90%]">
           {nfts.map((nft) => (
